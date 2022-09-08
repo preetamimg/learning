47ddecdf9e335ea8d30510f3e7f1d50e4b46a04f
jitendareorchids <jitendraeorchids@gmail.com>
2022-09-08 17:23:28 +0530

    IF ELSE CODE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learning';
+  data = ''
+  count = 0
+  type = 'password'
+  disable = true
+  hide = true
+  myName(name: string,rollNO:number){
+    alert(name);
+    alert(rollNO);
+  };
+  getData(val:any){
+    console.log(val)
+    this.data = val
+  };
+  counter(name:string){
+    name=== 'add' ? this.count++ : this.count--
+    if(this.count<0){
+      this.count = 0
+    }
+  };
+  show(){
+      this.type === 'password' ? this.type = 'text' : this.type = 'password';
+  };
+  getVal(val1:string){
+    console.log(val1)
+    val1=== '' ? this.disable = true : this.disable = false
+  }
+  ifclick(){
+    this.hide== true ? this.hide = false : this.hide = true
+    console.log(this.hide)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
