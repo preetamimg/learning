6da0049f14ad4b7b1ed2e74acc5714c40f1bdf68
jitendareorchids <jitendraeorchids@gmail.com>
2022-09-17 10:08:21 +0530

    pass data from parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'learning';
+  title = 'Send Data Parent To Child';
+  data:number = 10;
 }
