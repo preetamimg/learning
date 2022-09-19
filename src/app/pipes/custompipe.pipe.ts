b6be10f1e5f21fe20ecf3e12afb3baf9bf7bcc29
jitendareorchids <jitendraeorchids@gmail.com>
2022-09-19 16:20:22 +0530

    custom pipe

---
+++ src/app/pipes/custompipe.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'custompipe'
+})
+export class CustompipePipe implements PipeTransform {
+
+  transform(value: number, ...args: unknown[]): unknown {
+    return value*2;
+  }
+
+}
